c5b70c003f6bc666e91bd6c1f76d8a0b09ad142a
Roger Huba <rogerhuba@gmail.com>
2023-04-11 14:24:34 -0500

    add demo

---
+++ class-17/warmup/warmup.py
@@ -0,0 +1,45 @@
+"""
+Warm-Up Exercise
+Find the age range.
+
+Feature Tasks
+Given a Binary Tree filled with Person objects, where each Person has an age attribute, determine the age range.
+"""
+
+
+def get_age_range(tree):
+
+    def walk(root, youngest=0, oldest=0):
+
+        if not root:
+            return
+        if root.value.age < youngest: # May need to come back to 0
+            youngest = root.value.age
+        elif root.value.age > oldest:
+            oldest = root.value.age
+
+        # call my resucursion
+        youngest, oldest = walk(root.left, youngest, oldest)
+        youngest, oldest = walk(root.right, youngest, oldest)
+
+        return youngest, oldest
+
+    first_person = tree.root.value
+    youngest, oldest = walk(tree.root, first_person.age, first_person.age)
+
+    return oldest - youngest
+
+        #         50
+        #
+        #     60      18
+        #
+        # 21    34   34   34
+        #
+    # check if the current root value < or > youngest and oldets
+
+
+
+
+  return age_range
+
+
