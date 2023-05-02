8cb01c8426608974e44c288f3bdf5936d511d858
Roger Huba <rogerhuba@gmail.com>
2023-05-02 13:59:17 -0500

    add demo

---
+++ class-32/demo/things-application/things/views.py
@@ -0,0 +1,16 @@
+from rest_framework.generics import ListAPIView, RetrieveUpdateDestroyAPIView
+from .models import Thing
+from .serializer import ThingSerializer
+from .permissions import isOwnerOrReadOnly
+
+
+class ThingList(ListAPIView):
+    permission_classes = (isOwnerOrReadOnly,)
+    queryset = Thing.objects.all()
+    serializer_class = ThingSerializer
+
+
+class ThingDetail(RetrieveUpdateDestroyAPIView):
+    permission_classes = (isOwnerOrReadOnly,)
+    queryset = Thing.objects.all()
+    serializer_class = ThingSerializer
